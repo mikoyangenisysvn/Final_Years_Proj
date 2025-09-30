3e00cadd84f86e1fd5230b390f30bf5ed195c006
mikoyangenisysvn <132347154+mikoyangenisysvn@users.noreply.github.com>
2025-09-30 09:17:01 +0700

    Update COMPARATOR.v

--- RTL/COMPARATOR.v
+++ RTL/COMPARATOR.v
@@ -1,8 +1,8 @@
 module comparator #(
     parameter WIDTH = 16   // độ rộng counter và duty
 )(
-    input  wire [WIDTH-1:0] counter_value, // giá trị counter
-    input  wire [WIDTH-1:0] duty,          // duty cycle
+    input  wire [WIDTH-1:0] CCR, // giá trị counter
+    input  wire [WIDTH-1:0] CCR_COMPARE,          // duty cycle
     input  wire             enable,        // enable
     output wire             pwm_out        // tín hiệu PWM
 );
@@ -13,3 +13,4 @@ module comparator #(
                      : 1'b0;
 
 endmodule
+
