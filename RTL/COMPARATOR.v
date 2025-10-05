78fd52e397098b7bbb8cae552c0de4d547514911
mikoyangenisysvn <132347154+mikoyangenisysvn@users.noreply.github.com>
2025-10-05 20:54:17 +0700

    Update COMPARATOR.v

--- RTL/COMPARATOR.v
+++ RTL/COMPARATOR.v
@@ -1,28 +1,24 @@
-module comparator #(
-    parameter WIDTH = 16   // độ rộng counter và duty
+module pwm_comparator #(
+    parameter WIDTH = 16
 )(
-    input  wire [WIDTH-1:0] CCR, // giá trị counter
-    input  wire [WIDTH-1:0] CCR_COMPARE,          // duty cycle
-    input  wire [WIDTH-1:0] period,        // period (để check full duty)
-    input  wire             enable,        // enable
-    output wire             pwm_out        // tín hiệu PWM
+    input  wire [WIDTH-1:0] CNT, 
+    input  wire [WIDTH-1:0] CCR, 
+    input  wire [WIDTH-1:0] PERIOD,
+    input  wire             ENABLE,
+    output reg              PWM_OUT
 );
 
-   reg pwm_r;
-
-always @* begin
-    case (1'b1)
-        (!enable):         pwm_r = 1'b0;
-        (CCR_COMPARE == 0):       pwm_r = 1'b0;
-        (CCR_COMPARE >= period):  pwm_r = 1'b1;
-        (CCR < CCR_COMPARE): pwm_r = 1'b1;
-        default:           pwm_r = 1'b0;
-    endcase
-end
-
-assign pwm_out = pwm_r;
-
+    always @(*) begin
+        if (!ENABLE)
+            PWM_OUT = 1'b0;
+        else if (CCR == 0)
+            PWM_OUT = 1'b0;
+        else if (CCR >= PERIOD)
+            PWM_OUT = 1'b1;
+        else if (CNT < CCR)
+            PWM_OUT = 1'b1;
+        else
+            PWM_OUT = 1'b0;
+    end
 
 endmodule
-
-
