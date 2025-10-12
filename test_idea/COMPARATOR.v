31f028cc5ed5415ce6dbcc4dffb0ca6f7fc80997
mikoyangenisysvn <132347154+mikoyangenisysvn@users.noreply.github.com>
2025-10-12 10:30:33 +0700

    Update COMPARATOR.v

--- test_idea/COMPARATOR.v
+++ test_idea/COMPARATOR.v
@@ -1,8 +1,9 @@
 module pwm_comparator #(
     parameter WIDTH = 16
 )(
-    input  wire [WIDTH-1:0] CNT, 
-    input  wire [WIDTH-1:0] CCR, 
+    input  wire [WIDTH-1:0] CNT,
+    input  wire [WIDTH-1:0] CCR,
+    input  wire [WIDTH-1:0] DELAY,   // <--- thêm ngõ vào mới
     input  wire [WIDTH-1:0] PERIOD,
     input  wire             ENABLE,
     output reg              PWM_OUT
@@ -15,7 +16,7 @@ module pwm_comparator #(
             PWM_OUT = 1'b0;
         else if (CCR >= PERIOD)
             PWM_OUT = 1'b1;
-        else if (CNT < CCR)
+        else if ((CNT >= DELAY) && (CNT < (DELAY + CCR)))   // <--- logic có delay
             PWM_OUT = 1'b1;
         else
             PWM_OUT = 1'b0;
